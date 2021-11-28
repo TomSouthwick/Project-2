5b6162f34754b23c91d5fb51db3519df7f979240
Ygor Oliveira do Carmo <ygor.carmo@gmail.com>
2021-11-28 11:43:12 +1100

    added Post model

--- models/index.js
+++ models/index.js
@@ -1,8 +1,16 @@
 const User = require('./User');
+const Post = require('./Post');
 const Friendship = require('./Friendship');
 
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
 
 
-
-module.exports = { User ,Friendship };
+module.exports = { User, Post ,Friendship };
