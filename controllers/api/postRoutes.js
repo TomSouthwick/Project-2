1f7aeace96f3f55bed41c816788aabee898820a2
Ygor Oliveira do Carmo <ygor.carmo@gmail.com>
2021-12-04 17:46:25 +1100

    added login function

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -15,6 +15,25 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+router.put("/:id", withAuth, (req, res) => {
+  console.log(req.body, req.params.id)
+  Post.update(req.body, {
+    where: {
+      id: req.params.id,
+      user_id: req.session.user_id
+    }
+  }).then(affectedRows => {
+      if (affectedRows > 0) {
+        res.status(200).end();
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const postData = await Post.destroy({
