cf7717bfc1a7b44fe981a28a4ff17ac86c465475
Ygor Oliveira do Carmo <ygor.carmo@gmail.com>
2021-12-05 14:40:08 +1100

    fixed create post layout

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -45,24 +45,17 @@ router.get("/", async (req, res) => {
 
 router.get("/profile", withAuth, async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
-    const projectData = await Post.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ["first_name"],
-        },
-      ],
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }]
     });
 
-    // Serialize data so the template can read it
-    const posts = projectData.map((post) => post.get({ plain: true }));
-    console.log(posts);
+    const user = userData.get({ plain: true });
 
-    // Pass serialized data and session flag into template
-    res.render("profile", {
-      posts: posts,
-      logged_in: req.session.logged_in,
+    res.render('profile', {
+      ...user,
+      logged_in: true
     });
   } catch (err) {
     res.status(500).json(err);
